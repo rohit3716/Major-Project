ad2b741ead6102644ae005c0ccbdc07e8ab28d60
Rohit Raj <rohit.rajcs2020@indoreinstitute.com>
2023-10-17 23:59:23 +0530

    profile updated

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -1,5 +1,6 @@
 const Profile = require('../models/Profile');
 const User = require('../models/User');
+const { uploadImageToCloudinary } = require('../utils/imageUploader');
 
 //to update profile
 exports.updateProfile = async ( req, res ) => {
@@ -20,21 +21,21 @@ exports.updateProfile = async ( req, res ) => {
 
         //find profile
         const userDetails = await User.findById(id);
-        const profileId = userDetails.additionalDetails;
-        const profileDetails = await Profile.findById(profileId);
+        const profile = userDetails.additionalDetails;
 
         //update profile 
         profileDetails.dateOfBirth = dateOfBirth;
         profileDetails.about = about;
-        profileDetails.gender = gender;
         profileDetails.contactNumber = contactNumber;
+        
+        //save the updated profile
         await profileDetails.save();
         
         //return response
         return res.status(200).json({
             success:true,
             message:'Profile updated successfully',
-            profileDetails,
+            profile,
         });
     } catch (error) {
         console.log(error);
@@ -47,7 +48,7 @@ exports.updateProfile = async ( req, res ) => {
 
 //to delete profile
 //TODO: How to schedule deletion request?
-exports.deleteProfile = async ( req, res ) => {
+exports.deleteAccount = async ( req, res ) => {
    
     try {
          //get id
@@ -106,5 +107,65 @@ exports.getAllUserDetails = async ( req, res ) => {
             success:false,
             message:error.message,
         })
+    }
+}
+
+
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+        const displayPicture = req.files.displayPicture;
+        const userId = req.user.id;
+        const image = await uploadImageToCloudinary(
+            displayPicture,
+            process.env.FOLDER_NAME,
+            1000,
+            1000
+        )
+        console.log(image);
+
+        const updatedProfile = await User.findByIdAndUpdate(
+            {_id:userId},
+            {image: image.secure_url},
+            {new:true}
+        );
+
+        res.send({
+            success:true,
+            message:'Image uploaded successfully',
+            data: updatedProfile,
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+          })
+    }
+}
+
+exports.getEnrolledCourses = async (req, res ) => {
+    try {
+        const userId = req.user.id;
+        const userDetails = await User.findOne({
+            _id:userId,
+        })
+        .populate("courses")
+        .exec()
+
+        if( !userDetails ){
+            return res.status(400).json({
+                success: false,
+                message: `Could not find user with id: ${userDetails}`,
+              })
+        }
+        return res.status(200).json({
+            success: true,
+            data: userDetails.courses,
+          })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+          })
     }
 }
